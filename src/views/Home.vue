8f2e5beb92a8937581ddac50686b959b32ef40e3
PeterH <pete08@gmail.com>
2020-08-17 20:49:09 -0500

    Add library: mapbox to new app example_map_app, gettign startes, to the index.js, home.vue files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,92 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+
+      <div id="map"></div>
+
+    </div>
+
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+/* global mapboxgl */
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  
+  mounted: function() {
+    mapboxgl.accessToken = process.env.VUE_APP_mapbox_api;
+    var map = new mapboxgl.Map({
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [-87.5917, 41.7948],
+      zoom: 16,
+      pitch: 45,
+      bearing: -17.6,
+      container: 'map',
+      antialias: true
+    });
+
+    map.on('load', function() {
+      // Insert the layer beneath any symbol layer.
+      var layers = map.getStyle().layers;
+      
+      var labelLayerId;
+      for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+          labelLayerId = layers[i].id;
+          break;
+        }
+      } 
+    
+      map.addLayer(
+        {
+          'id': '3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',
+          'minzoom': 15,
+          'paint': {
+            'fill-extrusion-color': '#aaa',
+
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'height']
+            ],
+            'fill-extrusion-base': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'min_height']
+            ],
+            'fill-extrusion-opacity': 0.6
+          }
+        },
+        labelLayerId
+      );
+    });
+
+
+
+
+  },
+
+
+  methods: {}
+};
 </script>
